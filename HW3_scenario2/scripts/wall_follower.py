e6c0b31b239b83c1559bea4b0eaa6b92f0940b3b
Your Name <you@example.com>
2022-06-03 18:03:22 +0430

    PID Controller WallFollowing 1

--- HW3_scenario2/scripts/wall_follower.py
+++ HW3_scenario2/scripts/wall_follower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-from time import sleep
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
@@ -16,6 +15,13 @@ class Follower:
         self.default_distance = 0.5
         self.linear_speed = 0.2
         self.angular_speed = 0.2
+        self.dt = 0.005
+        self.epsilon = 2
+
+        # gains for angular PID controller
+        self.kp_a = 0.6
+        self.ki_a = 0
+        self.kd_a = 7
 
     def find_wall(self):
 
@@ -43,7 +49,7 @@ class Follower:
 
             wall_angle ,wall_distance = self.find_wall()
 
-            if (goal_angle - 2) <= wall_angle <= (goal_angle + 2):
+            if (goal_angle - 5) <= wall_angle <= (goal_angle + 5):
 
                 self.cmd_publisher.publish(Twist())
                 break
@@ -69,6 +75,35 @@ class Follower:
         
 
             
+    def wall_following(self):
+
+        print("wall_following has started")
+        twist = Twist()
+        sum_angular_error = 0
+        prev_angular_error = 0
+
+        while not rospy.is_shutdown():
+
+            wall_angle ,wall_distance = self.find_wall()
+            # angle_error = (270 - wall_angle) % 180
+            distance_error = self.default_distance - wall_distance 
+            angle_error = distance_error
+
+            sum_angular_error += (angle_error * self.dt)
+
+            P = self.kp_a * angle_error
+            I = self.ki_a * sum_angular_error
+            D = self.kd_a * (angle_error - prev_angular_error) 
+            twist.angular.z =  P + I + D
+
+            prev_angular_error = angle_error
+
+            twist.linear.x = self.linear_speed
+            self.cmd_publisher.publish(twist)
+            rospy.sleep(self.dt)
+
+
+
 
 
 
@@ -76,8 +111,8 @@ if __name__ == "__main__":
     
     follower = Follower()
 
-    # in order to reach the wall
     follower.reach_wall_first_time()
+    follower.wall_following()
     
     
     
