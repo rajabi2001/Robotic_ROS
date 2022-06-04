d3b6004056b51c6cc83c7311966c73edf4d15a10
Your Name <you@example.com>
2022-06-04 12:40:55 +0430

    Complete GoalFollower

--- HW3_scenario2/scripts/goal_follower.py
+++ HW3_scenario2/scripts/goal_follower.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 from time import sleep
+import tf
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 import numpy as np
+import math
 
 class Follower:
     
@@ -27,16 +29,74 @@ class Follower:
         self.ki_a = 0
         self.kd_a = 35
 
+        
 
+
+    # def update_odom_object(self):
+    #     self.odom_object = rospy.wait_for_message("/odom" , Odometry)
+
+
+    # heading of the robot 
+    def get_heading(self):
+        
+        # waiting for the most recent message from topic /odom#
+        msg = rospy.wait_for_message("/odom" , Odometry)
+        # msg = self.odom_object
+
+        orientation = msg.pose.pose.orientation
+        
+        # convert quaternion to odom
+        roll, pitch, yaw = tf.transformations.euler_from_quaternion((
+            orientation.x ,orientation.y ,orientation.z ,orientation.w
+        )) 
+        
+        return yaw
+
+    # position of the robot 
     def get_position(self):
 
         # waiting for the most recent message from topic /odom
         msg = rospy.wait_for_message("/odom" , Odometry)
+        # msg = self.odom_object
         
         position = msg.pose.pose.position
 
         x, y = float("{:.1f}".format(position.x)), float("{:.1f}".format(position.y))
         return x ,y
+
+    def find_angle(self):
+
+        robot_x ,robot_y = self.get_position()
+
+        angle = float("{:.2f}".format(np.arctan2((self.goal_y - robot_y), (self.goal_x - robot_x))))
+
+        return angle
+
+    def get_angular_error(self):
+
+        goal_orientaion = self.find_angle()
+        robot_orientation = self.get_heading()
+        if robot_orientation > 0:
+            sign = -1 if (robot_orientation - math.pi < goal_orientaion < robot_orientation) else +1
+        else:
+            sign = +1 if (robot_orientation + math.pi > goal_orientaion > robot_orientation) else -1
+
+        return sign * (math.pi - abs(abs(robot_orientation - goal_orientaion) - math.pi))
+
+    # def turn_to_goal(self):
+
+    #     robot_orientation = self.get_heading()
+    #     goal_orientation = self.find_angle()
+        
+    #     twist = Twist()
+    #     twist.angular.z = -1 * self.angular_speed
+    #     self.cmd_publisher.publish(twist)
+
+    #     while abs(robot_orientation - goal_orientation) >= 0.08 :
+    #         print(robot_orientation)
+    #         robot_orientation = self.get_heading()
+        
+    #     self.cmd_publisher.publish(Twist())
 
     def find_wall(self):
 
@@ -70,6 +130,7 @@ class Follower:
 
                 self.cmd_publisher.publish(Twist())
                 break
+   
 
     def reach_wall_first_time(self):
 
@@ -138,17 +199,42 @@ class Follower:
             rospy.sleep(self.dt)
 
 
+    def goal_following(self):
 
 
-    def reach_goal(self):
+        twist = Twist()
+        sum_angular_error = 0
+        prev_angular_error = 0
+
+        while not rospy.is_shutdown():
 
 
-        pass
+
+            # self.update_odom_object()
+            
+            angular_error = self.get_angular_error()
+
+            sum_angular_error += (angular_error * self.dt)
+
+
+            # calculate PID for angular speed
+            P = self.kp_a * angular_error
+            I = self.ki_a * sum_angular_error
+            D = self.kd_a * (angular_error - prev_angular_error) 
+            twist.angular.z =  P + I + D
+
+            prev_angular_error = angular_error
+
+            twist.linear.x = self.linear_speed
+            self.cmd_publisher.publish(twist)
+            rospy.sleep(self.dt)
 
 if __name__ == "__main__":
     
     follower = Follower()
 
+
+    follower.goal_following()
     # follower.reach_wall_first_time()
     # follower.wall_following()
     
