5e6a6147d772b3c03577c477d891f53e4f765620
Your Name <you@example.com>
2022-06-04 15:15:19 +0430

    Combine WallFollowing and GoalFollowing

--- HW3_scenario2/scripts/goal_follower.py
+++ HW3_scenario2/scripts/goal_follower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from time import sleep
+from turtle import distance
 import tf
 import rospy
 from sensor_msgs.msg import LaserScan
@@ -17,17 +18,23 @@ class Follower:
 
         self.cmd_publisher = rospy.Publisher("/cmd_vel" , Twist , queue_size=10)
 
-        self.default_distance = 0.375
-        self.linear_speed = 0.15
+        self.default_distance = 0.3
+        self.linear_speed = 0.12
         self.angular_speed = 0.2
         self.dt = 0.005
         self.goal_x = 3
         self.goal_y = -1
 
-        # gains for PID controller
-        self.kp_a = 3
-        self.ki_a = 0
-        self.kd_a = 35
+        # gains for angular PID Goal controller
+        self.kp_g = 2.6
+        self.ki_g = 0.001
+        self.kd_g = 0.9
+
+        # gains for PID Wall controller
+        self.kp_w = 2
+        self.ki_w = 0
+        self.kd_w = 22
+        # self.kp_l = 
 
         
 
@@ -64,6 +71,13 @@ class Follower:
         x, y = float("{:.1f}".format(position.x)), float("{:.1f}".format(position.y))
         return x ,y
 
+    def Euclidean_distance(self, x1, x2, y1, y2):
+
+        point1 = np.array((x1,y1))
+        point2 = np.array((x2,y2))
+
+        return np.linalg.norm(point1 - point2)
+
     def find_angle(self):
 
         robot_x ,robot_y = self.get_position()
@@ -83,20 +97,6 @@ class Follower:
 
         return sign * (math.pi - abs(abs(robot_orientation - goal_orientaion) - math.pi))
 
-    # def turn_to_goal(self):
-
-    #     robot_orientation = self.get_heading()
-    #     goal_orientation = self.find_angle()
-        
-    #     twist = Twist()
-    #     twist.angular.z = -1 * self.angular_speed
-    #     self.cmd_publisher.publish(twist)
-
-    #     while abs(robot_orientation - goal_orientation) >= 0.08 :
-    #         print(robot_orientation)
-    #         robot_orientation = self.get_heading()
-        
-    #     self.cmd_publisher.publish(Twist())
 
     def find_wall(self):
 
@@ -106,6 +106,7 @@ class Follower:
         laser_msg = rospy.wait_for_message("/scan" , LaserScan)
 
         iterate_list = list(range(0,6)) + list(range(180,360))
+        # iterate_list = list(range(0,360))
         for i in iterate_list:
 
             if laser_msg.ranges[i] < min_distance:
@@ -129,35 +130,26 @@ class Follower:
             if (goal_angle - 3) <= wall_angle <= (goal_angle + 3):
 
                 self.cmd_publisher.publish(Twist())
+                sleep(1)
                 break
-   
 
-    def reach_wall_first_time(self):
+    def turn_right(self):
 
+        goal_angle = 270
         twist = Twist()
-        twist.linear.x = self.linear_speed
+        twist.angular.z = -1 * self.angular_speed
+        self.cmd_publisher.publish(twist)
 
-        while True:
-
-            self.cmd_publisher.publish(twist)
+        while True :
 
             wall_angle ,wall_distance = self.find_wall()
 
-            if wall_distance <= (self.default_distance + 0.05):
+            if (goal_angle - 3) <= wall_angle <= (goal_angle + 3):
 
-                print(f"angle:{wall_angle} and distance:{wall_distance}")
                 self.cmd_publisher.publish(Twist())
-                break
-        
-        self.turn_left()
-        
-        twist = Twist()
-        twist.linear.x = 0.5
-        self.cmd_publisher.publish(Twist())
-        sleep(1)
-        
-
-            
+                sleep(1)
+                break   
+         
     def wall_following(self):
 
         print("wall_following has started")
@@ -165,33 +157,31 @@ class Follower:
         sum_angular_error = 0
         prev_angular_error = 0
 
-        while not rospy.is_shutdown():
+        while True:
 
-            checklist = list(range(0,5)) + list(range(355,360))
+            robot_orientation = self.get_heading()
+            print(robot_orientation)
+            if -1.65 < robot_orientation < -1.48:
+                print("fuuuuuuuuuuuck")
+                twist = Twist()
+                twist.linear.x = self.linear_speed 
+                self.cmd_publisher.publish(twist)
+                sleep(2)
+                break
 
             wall_angle ,wall_distance = self.find_wall()
-            # print(f"{wall_angle} , {wall_distance}")
-            if wall_angle in checklist:
-                
-                self.turn_left()
-                twist = Twist()
-                twist.linear.x = 0.1
-                self.cmd_publisher.publish(Twist())
-                sleep(1)
-                continue
-
             distance_error = self.default_distance - wall_distance
 
             distance_error = self.default_distance - wall_distance
             sum_angular_error += (distance_error * self.dt)
 
             # P = self.kp_a * distance_error + abs(self.kp_l * angle_error)
-            P = self.kp_a * distance_error
-            I = self.ki_a * sum_angular_error
-            D = self.kd_a * (distance_error - prev_angular_error) 
+            P = self.kp_w * distance_error
+            I = self.ki_w * sum_angular_error
+            D = self.kd_w * (distance_error - prev_angular_error) 
 
             twist.angular.z =  P + I + D
-            # twist.linear.x = self.linear_speed - abs(self.kp_l * angle_error)
+            # twist.linear.x = self.linear_speed - abs(self.kp_l * distance_error)
             twist.linear.x = self.linear_speed
 
             prev_angular_error = distance_error
@@ -206,8 +196,14 @@ class Follower:
         sum_angular_error = 0
         prev_angular_error = 0
 
-        while not rospy.is_shutdown():
+        while True:
 
+            
+            wall_angle ,wall_distance = self.find_wall()
+            if wall_distance <= 0.4:
+                self.cmd_publisher.publish(Twist())
+                sleep(1)
+                break
 
 
             # self.update_odom_object()
@@ -218,9 +214,9 @@ class Follower:
 
 
             # calculate PID for angular speed
-            P = self.kp_a * angular_error
-            I = self.ki_a * sum_angular_error
-            D = self.kd_a * (angular_error - prev_angular_error) 
+            P = self.kp_g * angular_error
+            I = self.ki_g * sum_angular_error
+            D = self.kd_g * (angular_error - prev_angular_error) 
             twist.angular.z =  P + I + D
 
             prev_angular_error = angular_error
@@ -229,14 +225,22 @@ class Follower:
             self.cmd_publisher.publish(twist)
             rospy.sleep(self.dt)
 
+    def run(self):
+
+        while not rospy.is_shutdown():
+
+            self.goal_following()
+            self.turn_left()
+            self.wall_following()
+
+
+
 if __name__ == "__main__":
     
     follower = Follower()
 
 
-    follower.goal_following()
-    # follower.reach_wall_first_time()
-    # follower.wall_following()
+    follower.run()
     
     
     
